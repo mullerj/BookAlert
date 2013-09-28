b268ca84be4c88618205c2cb19a8aab94a2719c0
DanielBarlow <webmaster@thedwebsite.net>
2013-09-28 17:17:50 -0400

    Got real bulkadd.php from server

--- www/bulkadd.php
+++ www/bulkadd.php
@@ -1,4 +1,43 @@
-ISBN not provided<script>
+<?php
+if (isset($_GET['add'])){
+echo "You provided an ISBN<p>";
+$isbn =	$_GET['add']/1;
+#$xmldat = simplexml_load_file('http://isbndb.com/api/books.xml?access_key=PEM3592Y&results=subjects&index1=isbn&value1='.$isbn);
+$xmldat = simplexml_load_file('ex.xml');
+$title = $xmldat->BookList->BookData->Title;
+$author = $xmldat->BookList->BookData->AuthorsText;
+
+/*$xmldat->BookList->BookData->Subjects->Subject[0];*/
+$con=mysqli_connect("127.0.0.1","root","root","bookalert");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$result=mysqli_query($con,"SELECT quantity FROM inventory WHERE isbn = ".$isbn);
+$quantity = 1;
+while($row = mysqli_fetch_array($result))
+  {
+  $quantity=$row['quantity']+1;
+  echo "<br>Quantity: ".$quantity;
+  }
+if($quantity==1){
+mysqli_query($con,"INSERT INTO inventory VALUES ( ".$isbn." , '".$title."' , '".$author."' , ".$quantity." );");
+}
+else {
+mysqli_query($con,"UPDATE inventory SET quantity=".$quantity." WHERE isbn = ".$isbn." );");
+}
+mysqli_close($con);
+
+echo "<!--";
+print_r($xmldat);
+echo "-->";
+} else {
+echo "ISBN not provided";
+}
+?>
+<script>
 setTimeout('location="http://zxing.appspot.com/scan?ret=http://bookalert.me/bulkadd?add={CODE}";',10000);
 </script><p>
 <a href="http://zxing.appspot.com/scan?ret=http://bookalert.me/bulkadd?add={CODE}">SCAN NOW</a>
