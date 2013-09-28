1e5e42bf444edd077cda1bfce0e701b65e101b0b
DanielBarlow <webmaster@thedwebsite.net>
2013-09-28 17:11:37 -0400

    Added Web File, bulkadd, and database dump

---
+++ www/bulkadd.php
@@ -0,0 +1,4 @@
+ISBN not provided<script>
+setTimeout('location="http://zxing.appspot.com/scan?ret=http://bookalert.me/bulkadd?add={CODE}";',10000);
+</script><p>
+<a href="http://zxing.appspot.com/scan?ret=http://bookalert.me/bulkadd?add={CODE}">SCAN NOW</a>
